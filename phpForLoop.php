cb3b94dd0e2bf1ace5954eb29fde55b029eb7286
Eoin O'Kennedy <eoin.okennedy@tudublin.ie>
2023-10-04 09:19:43 +0100

    changed the for loop to count to 20

--- phpForLoop.php
+++ phpForLoop.php
@@ -1,9 +1,8 @@
 <?php
-#phpForLoop - put the name of the file/program here - notes the hashtag indicates a comment in PHP
-#put your name and the date here
-#put a descripition of what the code does here - 
-#this program uses a loop to count up to 10 and puts the value of the loop counter out to the screen on each iteration
-for ($i=1;$i<=10;$i++) {
+#phpForLoop
+#Eoin O'Kennedy 4/10/2023
+#this program uses a loop to count up to 20 and puts the value of the loop counter out to the screen on each iteration
+for ($i=1;$i<=20;$i++) {
     echo "$i, ";
 }
 
